fc1735524b5f15f1c01d55eedb3c876b7d454ffb
Jake Lauterstein <97852239+jakelauterstein@users.noreply.github.com>
2022-06-06 16:51:11 -0500

    addedvoting functionality

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,19 @@
+const sequelize = require('../../config/connection');
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
 
-// get all users
+
+// get all posts
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
-        attributes: ['id', 'post_url', 'title', 'created_at'],    
-        order: [['created_at', 'DESC']], 
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+          ],        order: [['created_at', 'DESC']], 
         include: [
             {
               model: User,
@@ -23,12 +30,19 @@ router.get('/', (req, res) => {
 
   });
 
+  //get one post
   router.get('/:id', (req, res) => {
     Post.findOne({
       where: {
         id: req.params.id
       },
-      attributes: ['id', 'post_url', 'title', 'created_at'],
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
       include: [
         {
           model: User,
@@ -49,6 +63,7 @@ router.get('/', (req, res) => {
       });
   });
 
+  // create post
   router.post('/', (req, res) => {
     // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
     Post.create({
@@ -63,6 +78,19 @@ router.get('/', (req, res) => {
       });
   });
 
+
+  // PUT /api/posts/upvote
+  router.put('/upvote', (req, res) => {
+    // custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+      .then(updatedPostData => res.json(updatedPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  });
+
+//update post
   router.put('/:id', (req, res) => {
     Post.update(
       {
